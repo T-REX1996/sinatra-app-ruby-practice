b2f0fe2fb03bde88cf69d7ac18352233983145c9
T-REX1970 <bump12@icloud.com>
2026-09-12 17:35:30 +0900

    Sinatraでメモ帳アプリを実装

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # sinatra-app-ruby-practice
+
+Sinatraで作成したシンプルなメモ帳アプリです。フィヨルドブートキャンプの「SinatraでシンプルなWebアプリを作ろう」の課題として作成しました。
+
+## 機能
+
+- メモの一覧表示
+- メモの新規作成
+- メモの詳細表示
+- メモの編集
+- メモの削除
+
+## 使用技術
+
+- Ruby
+- Sinatra
+- ERB
+- JSON(データ保存)
+
+## ローカルでの起動方法
+
+### 1. リポジトリをクローン
+
+    git clone https://github.com/あなたのユーザー名/sinatra-app-ruby-practice.git
+    cd sinatra-app-ruby-practice
+
+### 2. Bundlerで必要なGemをインストール
+
+    bundle install
+
+### 3. アプリを起動
+
+    bundle exec ruby app.rb
+
+### 4. ブラウザでアクセス
+
+以下のURLにアクセスしてください。
+
+    http://localhost:4567/memos
---
+++ app.rb
@@ -0,0 +1,83 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'sinatra/contrib'
+require 'json'
+
+use Rack::MethodOverride
+
+set :erb, escape_html: true
+
+DATA_FILE = File.join(__dir__, 'data', 'memos.json')
+
+def load_memos
+  JSON.parse(File.read(DATA_FILE))
+end
+
+get '/memos' do
+  @memos = load_memos
+  erb :index
+end
+
+def save_memos(memos)
+  File.write(DATA_FILE, JSON.pretty_generate(memos))
+end
+
+def next_id(memos)
+  return 1 if memos.empty?
+
+  memos.map { |memo| memo['id'] }.max + 1
+end
+
+get '/memos/new' do
+  erb :new
+end
+
+post '/memos' do
+  memos = load_memos
+  new_memo = {
+    'id' => next_id(memos),
+    'title' => params[:title],
+    'body' => params[:body],
+    'created_at' => Time.now.iso8601
+  }
+  memos << new_memo
+  save_memos(memos)
+  redirect '/memos'
+end
+
+def find_memo(id)
+  memos = load_memos
+  memos.find { |memo| memo['id'] == id.to_i }
+end
+
+get '/memos/:id' do
+  @memo = find_memo(params[:id])
+  erb :show
+end
+
+get '/memos/:id/edit' do
+  @memo = find_memo(params[:id])
+  erb :edit
+end
+
+patch '/memos/:id' do
+  memos = load_memos
+  memo = memos.find { |m| m['id'] == params[:id].to_i }
+  memo['title'] = params[:title]
+  memo['body'] = params[:body]
+  save_memos(memos)
+  redirect "/memos/#{params[:id]}"
+end
+
+delete '/memos/:id' do
+  memos = load_memos
+  memos.reject! { |m| m['id'] == params[:id].to_i }
+  save_memos(memos)
+  redirect '/memos'
+end
+not_found do
+  status 404
+  erb :not_found
+end
